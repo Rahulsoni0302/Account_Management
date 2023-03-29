e61ca7621bb094df234d1d9cc8f6985d7b641887
Rahul soni <sonishiva2002@gmail.com>
2023-03-30 00:43:05 +0530

    repository created ,we can begin our work now

---
+++ src/main/java/com/mycompany/account_management/Account_Management.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.account_management;
+
+/**
+ *
+ * @author Rahul
+ */
+public class Account_Management {
+
+    public static void main(String[] args) {
+       
+    }
+}
